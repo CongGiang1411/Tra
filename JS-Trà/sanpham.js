2488ac39f87fd1367cd327e0d76bceb232800a52
giang <giang@gmail.com>
2023-05-31 21:58:46 +0700

    commit lan 1

---
+++ JS-Trà/sanpham.js
@@ -0,0 +1,11 @@
+function back(){
+    window.history.back();
+}
+function trang1(){
+    document.getElementById("fieldset1").style.display="block";
+    document.getElementById("fieldset2").style.display="none";
+}
+function trang2(){
+    document.getElementById("fieldset1").style.display="none";
+    document.getElementById("fieldset2").style.display="block";
+}
